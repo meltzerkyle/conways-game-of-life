8ff18dc9500982a1dd880de1a00a20fb7b35dd87
meltzerkyle <meltzerkyle@gmail.com>
2018-12-18 16:19:05 -0600

    Updates

--- precache-manifest.10e9365e3b4788feceba5f72a9442024.js
+++ precache-manifest.10e9365e3b4788feceba5f72a9442024.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/conways-game-of-life/static/js/runtime~main.be9fb68f.js"
   },
   {
-    "revision": "d15c53aa8e0313cf6029",
-    "url": "/conways-game-of-life/static/js/main.d15c53aa.chunk.js"
+    "revision": "9e45d8ec27946b9af0a1",
+    "url": "/conways-game-of-life/static/js/main.9e45d8ec.chunk.js"
   },
   {
     "revision": "882bd5e9d8877477a7d2",
     "url": "/conways-game-of-life/static/js/1.882bd5e9.chunk.js"
   },
   {
-    "revision": "d15c53aa8e0313cf6029",
-    "url": "/conways-game-of-life/static/css/main.ef4f6a21.chunk.css"
+    "revision": "9e45d8ec27946b9af0a1",
+    "url": "/conways-game-of-life/static/css/main.8eef33f9.chunk.css"
   },
   {
     "revision": "882bd5e9d8877477a7d2",
     "url": "/conways-game-of-life/static/css/1.d6d7660d.chunk.css"
   },
   {
-    "revision": "047355fa9fefb3e41158067424e04da3",
+    "revision": "ff79d211793ccce21741535613e8dbe9",
     "url": "/conways-game-of-life/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/conways-game-of-life/precache-manifest.514315a0e361fa90cb11bedcf49bcff9.js"
+  "/conways-game-of-life/precache-manifest.10e9365e3b4788feceba5f72a9442024.js"
 );
 
 workbox.clientsClaim();
